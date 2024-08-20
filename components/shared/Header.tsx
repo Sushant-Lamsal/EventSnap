e4d78adc7f9f6f4744e16f7dade2f486a97769a2
Sushant <sushantlamsal31@gmail.com>
2024-08-20 18:23:15 +0545

    Login

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
           </SignedIn>
           <SignedOut>
           <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-purple-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-              <Link href="/sign-in">Login</Link>
+              <Link href="/sign-in">LOGIN</Link>
             </button>
           </SignedOut>
         </div>
