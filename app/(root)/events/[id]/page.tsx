bf4e58300a7389967917ca4957ede1ea0ac95559
Sushant <sushantlamsal31@gmail.com>
2024-08-24 17:55:22 +0545

    Feat : styling changes

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -7,6 +7,7 @@ import {
 import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
 
 const EventDetails = async ({
   params: { id },
@@ -22,13 +23,13 @@ const EventDetails = async ({
   return (
     <>
       <section className="flex justify-center  bg-dotted-pattern bg-contain">
-        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl ">
           <Image
             src={event.imageUrl}
             alt="hero image"
-            width={1000}
-            height={1000}
-            className="h-full min-h-[300px] object-cover object-center"
+            width={800}
+            height={800}
+            className="h-full min-h-[200px] object-cover object-center rounded-2xl"
           />
 
           <div className="flex w-full flex-col gap-8 bg-black text-white p-5 md:p-10">
@@ -37,16 +38,16 @@ const EventDetails = async ({
 
               <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
                 <div className="flex gap-3">
-                  <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
+                  <p className="p-bold-20 rounded-full bg-green-800 px-5 py-2 text-white">
                     {event.isFree ? "FREE" : `$${event.price}`}
                   </p>
-                  <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
+                  <p className="p-medium-16 rounded-full bg-grey-600 px-4 py-2.5 text-white">
                     {event.category.name}
                   </p>
                 </div>
 
                 <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
-                  by{" "}
+                  By{" ~  "}
                   <span className="text-primary-500">
                     {event.organizer.firstName} {event.organizer.lastName}
                   </span>
@@ -88,10 +89,10 @@ const EventDetails = async ({
             </div>
 
             <div className="flex flex-col gap-2">
-              <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
+              <p className="p-bold-20 text-grey-500">What You'll Learn:</p>
               <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
               <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
-                {event.url}
+               <Link target="blank" href={event.url} className="text-blue-600">{event.url}</Link>
               </p>
             </div>
           </div>
@@ -99,8 +100,8 @@ const EventDetails = async ({
       </section>
 
       {/* EVENTS with the same category */}
-      <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Related Events</h2>
+      <section className="wrapper my-8 flex flex-col gap-8 md:gap-12 text-white">
+        <h2 className="h2-bold">Related <span className="text-purple-600">Events</span> </h2>
 
         <Collection
           data={relatedEvents?.data}
--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -34,11 +34,17 @@ const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
 
   return (
     <form action={onCheckout} method="post">
-      <Button type="submit" role="link" size="lg" className="button sm:w-fit">
-        {event.isFree ? 'Get Ticket' : 'Buy Ticket'}
-      </Button>
+      <button type="submit" className="relative inline-flex h-12 overflow-hidden rounded-full p-[2px]">
+<span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+<span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+{event.isFree ? 'Get Ticket' : 'Buy Ticket'}
+</span>
+</button>
     </form>
   )
 }
 
 export default Checkout
+
+
+        
