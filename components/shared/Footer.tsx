7c98d83cffaedd212ef6f6e504b4e5e026871ada
Sushant <sushantlamsal31@gmail.com>
2024-08-24 14:13:03 +0545

    Style : Top and buttom border removed

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="border-t bg-black text-white border-purple-700">
+    <footer className=" bg-black text-white ">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href="/">
           <Image
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,7 +7,7 @@ import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <header className="w-full border-b border-purple-700 bg-black text-white">
+    <header className="w-full bg-black text-white">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image
