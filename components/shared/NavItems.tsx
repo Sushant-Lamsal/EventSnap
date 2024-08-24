ae2c3b54c0afc60e0c0e20eb3fe12e80fc15ec21
Sushant <sushantlamsal31@gmail.com>
2024-08-24 14:08:27 +0545

    Fix : Hover header style changes

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -17,8 +17,8 @@ const NavItems = () => {
           <li
             key={link.route}
             className={`${
-              isActive && 'text-primary-500'
-            } flex-center p-medium-16 whitespace-nowrap`}
+              isActive && 'text-purple-400 '
+            } flex-center p-medium-16 whitespace-nowrap hover:text-purple-400`}
           >
             <Link href={link.route}>{link.label}</Link>
           </li>
