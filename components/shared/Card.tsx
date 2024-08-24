39d7e487d8b2f1c854eb1b698a90cde55b550495
Sushant <sushantlamsal31@gmail.com>
2024-08-24 15:13:56 +0545

    Feat : Cards

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,3 +1,4 @@
+"use server"
 import { IEvent } from "@/lib/database/models/event.model";
 import { formatDateTime } from "@/lib/utils";
 import { auth } from "@clerk/nextjs";
@@ -5,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { DeleteConfirmation } from "./DeleteConfirmation";
+import { CardSpotlight } from "../ui/card-spotlight";
 
 type CardProps = {
   event: IEvent;
@@ -19,11 +21,12 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
   const isEventCreator = userId === event.organizer._id.toString();
 
   return (
-    <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
+    <CardSpotlight className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl  md:min-h-[438px]">
+    <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xlshadow-md transition-all hover:shadow-lg md:min-h-[438px]">
       <Link
         href={`/events/${event._id}`}
         style={{ backgroundImage: `url(${event.imageUrl})` }}
-        className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
+        className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500 rounded-xl"
       />
       {/* IS EVENT CREATOR ... */}
 
@@ -42,31 +45,31 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         </div>
       )}
 
-      <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4 bg-slate-950">
+      <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4 ">
         {!hidePrice && (
           <div className="flex gap-2">
-            <span className="p-semibold-14 w-min rounded-full bg-green-400 text-white px-4 py-1 text-green-60">
+            <span className="p-semibold-14 w-min rounded-full bg-green-800 text-white px-4 py-1 ">
               {event.isFree ? "FREE" : `$${event.price}`}
             </span>
-            <p className="p-semibold-14 w-min rounded-full bg-slate-800 px-4 py-1 text-white line-clamp-1">
+            <p className="p-semibold-14 w-min rounded-full bg-purple-800 px-4 py-1 text-white line-clamp-1">
               {event.category.name}
             </p>
           </div>
         )}
 
-        <p className="p-medium-16 p-medium-18 text-white">
+        <p className=" text-base text-slate-200 ">
           {formatDateTime(event.startDateTime).dateTime}
         </p>
 
         <Link href={`/events/${event._id}`}>
-          <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-white">
+          <p className="text-2xl font-thin line-clamp-2 flex-1 text-purple-400">
             {event.title}
           </p>
         </Link>
 
         <div className="flex-between w-full">
-          <p className="p-medium-14 md:p-medium-16 text-white">
-            {event.organizer.firstName} {event.organizer.lastName}
+          <p className="p-medium-14 md:p-medium-16 text-slate-100">
+            {' - '}{event.organizer.firstName} {event.organizer.lastName}
           </p>
 
           {hasOrderLink && (
@@ -83,6 +86,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         </div>
       </div>
     </div>
+    </CardSpotlight>
   );
 };
 
